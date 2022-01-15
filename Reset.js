e4345f6234220b7f6276987bc9c0732384dd1c6b
CCID <62042406+CCIDmods@users.noreply.github.com>
2022-01-14 21:40:27 -0800

    Create Reset.js

---
+++ Reset.js
@@ -0,0 +1,9 @@
+  DOC.Client.Clear();
+        } else {
+            DOC.Client.x();
+            if (import.Client + { xConfig } + from + "Xmodule") {
+
+                MediaSource.Client.Clear();
+            }
+            DOC.Client.Clear.x();
+        }
